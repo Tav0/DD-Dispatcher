18f1a2cbdb507d2d820c87e099e329106a3894b9
Woo Jin Kye <woojinkye@Wooui-MacBook-Pro.local>
2017-02-27 17:18:53 -0500

    initial hubspace

---
+++ DDDispatcher/ExtraInfoViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ExtraInfoViewController.swift
+//  DDDispatcher
+//
+//  Created by macbookair11 on 2/18/17.
+//  Copyright © 2017 DD Dispatcher. All rights reserved.
+//
+
+import UIKit
+
+class ExtraInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
